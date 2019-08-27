73ca519d9f74997abc3587095e5219b374351dea
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2019-08-27 13:31:48 +0200

    Add @responsive helper to generate responsive CSS
    When creating utilities you sometimes want to have them generated in a
    couple of variants for different screens. To prevent we have to do all
    of this by hand I created a @responsive helper that you can use to wrap
    the css in. All the classes inside that block will then get the
    proper breakpoints generated. Keep in mind that the original block will
    also always remain present.

--- postcss.config.js
+++ postcss.config.js
@@ -6,6 +6,15 @@ module.exports = {
     require("postcss-preset-env")(),
     require("postcss-nested"),
     require("postcss-extend-rule"),
-    require("postcss-reporter")({ clearReportedMessages: true })
+    require("postcss-reporter")({ clearReportedMessages: true }),
+    require("./plugins/atResponsive.js")({
+      divider: "\\:",
+      breakpoints: [
+        { name: "sm", breakpoint: "25rem" },
+        { name: "md", breakpoint: "40rem" },
+        { name: "lg", breakpoint: "60rem" },
+        { name: "xlg", breakpoint: "75rem" }
+      ]
+    })
   ]
 }
